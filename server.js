206e91625684fd1fad1838c948b493dd030f2a06
Prasanna Annamalai <prannamalai.x.com@gmail.com>
2012-11-16 14:08:57 +0530

    changed port to 80

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-app.listen(3000, function(){
+app.listen(80, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
